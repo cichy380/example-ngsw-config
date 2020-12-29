c9032ff35ce6a7f2fe4500e8fab1065d675a2fb4
cichy380 <marcin.dobroszek@gmail.com>
2020-12-29 17:46:53 +0100

    add method getData()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,10 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.getData();
+  }
+
+  public getData(): void {
     this.http.get('https://random-data-api.com/api/name/random_name').subscribe((data: any) => this.name = data.name);
     this.http.get('https://random-data-api.com/api/vehicle/random_vehicle').subscribe((data: any) => this.vehicle = data.make_and_model);
     this.http.get('https://random-data-api.com/api/food/random_food').subscribe((data: any) => this.food = data.dish);
